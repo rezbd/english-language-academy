cddf4fa1685412c925d6bebbf105342cb6b470f0
rezwan <rezwan6@gmail.com>
2021-10-04 21:15:13 +0600

    updated error message

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -3,8 +3,14 @@ import './Error.css';
 
 const Error = () => {
     return (
-        <div>
-            <h2 className="error-msg">404 Not Found!</h2>
+        <div className="my-5">
+            <div>
+                <img className="w-50" src="https://blog.fluidui.com/content/images/2019/01/imageedit_1_9273372713.png" alt="" />
+            </div>
+            <div>
+                <h2 className="text-danger fs-1 py-4">Oops!</h2>
+                <p className="fs-4 pt-1 pb-4">The page you are looking for is not available</p>
+            </div>
         </div>
     );
 };
